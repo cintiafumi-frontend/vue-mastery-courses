3ac4aaba04a020d9e8ebaf6246cf30841151e8f0
cintiafumi <cintiafumi@gmail.com>
2020-03-15 22:08:58 -0300

    unit-test: vue mastery unit testing vue course using jest

---
+++ unit-testing-vue/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppHeader from "@/components/AppHeader";
+
+export default {
+  name: "Home",
+  components: {
+    AppHeader
+  }
+};
+</script>
